9d2d857f0d774e04d77df2d3445111dadc93a334
salvigc98 <salvigc98@gmail.com>
2020-04-15 10:55:37 +0200

    add toastt

--- app/src/main/java/com/example/android/toolbarexample/MainActivity.kt
+++ app/src/main/java/com/example/android/toolbarexample/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.android.toolbarexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +13,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setSupportActionBar(topAppBar)
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+            R.id.action_chat -> {
+                Toast.makeText(this, "Este es mi chat", Toast.LENGTH_SHORT).show()
+            }
+            R.id.action_info -> {
+                Toast.makeText(this, "Esta es mi info", Toast.LENGTH_SHORT).show()
+            }
+            R.id.action_plus -> {
+                Toast.makeText(this, "mas opciones", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
